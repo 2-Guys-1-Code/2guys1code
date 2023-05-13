f9b717124a3c1f470c48178011c2f57aa12a7d87
Fab Mongeau <fabrice_mongeau@hotmail.com>
2023-05-13 10:04:11 -0400

    Adding to the game table

--- src/poker_pkg/game_table.py
+++ src/poker_pkg/game_table.py
@@ -2,15 +2,60 @@ class AlreadySeated(Exception):
     pass
 
 
-class GameTable:
-    def __init__(self) -> None:
-        self._seats: list[any] = []
+class TableIsFull(Exception):
+    pass
 
-    def join(self, entity: any):
-        if any([x for x in self._seats if x is entity]):
+
+class SeatIsTaken(Exception):
+    pass
+
+
+class InvalidSeat(Exception):
+    pass
+
+
+class CannotCreateTable(Exception):
+    pass
+
+
+class GameTable:
+    def __init__(self, size: int = 0) -> None:
+        if not isinstance(size, int) or size < 0:
+            raise CannotCreateTable()
+
+        self._seats: list[any] = [None] * size
+
+    def get_seat(self, entity: any) -> int:
+        for key, value in enumerate(self._seats):
+            if value is entity:
+                return key + 1
+
+        return None
+
+    def _join(self, entity: any, seat_number: int) -> None:
+        if self.get_seat(entity) is not None:
             raise AlreadySeated()
 
-        self._seats.append(entity)
+        if seat_number is None:
+            raise TableIsFull()
+
+        if self.get_at_seat(seat_number) is not None:
+            raise SeatIsTaken()
+
+        self._seats[seat_number - 1] = entity
+
+    def join(self, entity: any, **kwargs) -> None:
+        seat_number = next((i + 1 for i, x in enumerate(self._seats) if x is None), None)
+
+        self._join(entity, seat_number)
 
     def get_at_seat(self, seat_number: int):
+        if seat_number is None or seat_number < 0 or seat_number > len(self._seats):
+            raise InvalidSeat()
+
         return self._seats[seat_number - 1]
+
+
+class FreePickTable(GameTable):
+    def join(self, entity: any, seat: int) -> None:
+        self._join(entity, seat_number=seat)
--- tests/poker_pkg/test_table.py
+++ tests/poker_pkg/test_table.py
@@ -1,9 +1,23 @@
 import pytest
-from game_table import AlreadySeated, GameTable
+from game_table import (
+    AlreadySeated,
+    CannotCreateTable,
+    FreePickTable,
+    GameTable,
+    InvalidSeat,
+    SeatIsTaken,
+    TableIsFull,
+)
+
+
+@pytest.mark.parametrize("size_param", [-1, None, "string"])
+def test_create_invalid_table(size_param):
+    with pytest.raises(CannotCreateTable):
+        GameTable(size_param)
 
 
 def test_join_sequential():
-    table = GameTable()
+    table = GameTable(1)
 
     thing_1 = 1
     table.join(thing_1)
@@ -11,11 +25,72 @@ def test_join_sequential():
     assert table.get_at_seat(1) == thing_1
 
 
-def test_join__can_not_join_twice():
-    table = GameTable()
+def test_join_sequential__can_not_join_twice():
+    table = GameTable(1)
 
-    thing_1 = 1
+    thing_1 = object()
     table.join(thing_1)
 
     with pytest.raises(AlreadySeated):
         table.join(thing_1)
+
+
+def test_join_sequential__join_second():
+    table = GameTable(2)
+
+    thing_1 = 1
+    table.join(thing_1)
+
+    thing_2 = 2
+    table.join(thing_2)
+
+    assert table.get_at_seat(1) == thing_1
+    assert table.get_at_seat(2) == thing_2
+
+
+def test_join_sequential__table_is_full():
+    table = GameTable(size=0)
+
+    thing_1 = 1
+    with pytest.raises(TableIsFull):
+        table.join(thing_1)
+
+
+def test_pick_seat():
+    table = FreePickTable(size=3)
+
+    thing_1 = 1
+    table.join(thing_1, seat=2)
+
+    assert table.get_at_seat(2) == thing_1
+
+
+def test_pick_seat__already_seated():
+    table = FreePickTable(size=3)
+
+    thing_1 = 1
+    table.join(thing_1, seat=2)
+
+    with pytest.raises(AlreadySeated):
+        table.join(thing_1, seat=3)
+
+
+def test_pick_seat__seat_taken():
+    table = FreePickTable(size=3)
+
+    thing_1 = 1
+    table.join(thing_1, seat=2)
+
+    thing_2 = 2
+    with pytest.raises(SeatIsTaken):
+        table.join(thing_2, seat=2)
+
+
+@pytest.mark.parametrize(
+    "seat_number, seats_in_table", [(-1, 2), (4, 3)], ids=["negative seat", "seat larger than max"]
+)
+def test_pick_seat__invalid_seat_raises_exception(seat_number, seats_in_table):
+    table = FreePickTable(size=seats_in_table)
+    thing = 1
+    with pytest.raises(InvalidSeat):
+        table.join(thing, seat_number)
